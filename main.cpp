173edc184c4248ea6f63c78b9980eed1406c5d33
Ramir Sultanov <ramirrsultanov@stud.kpfu.ru>
2020-11-12 00:37:15 +0300

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-  int* arr = new int[5]{ 4, 3, 2, 5, 1 };
+  int arr[] = { 4, 3, 2, 5, 1 };
   std::cout << Task1::minElement(arr, 5) << std::endl;
   std::cout << Task2::minElement(&arr[0], &arr[4]) << std::endl;
   Task3::rotate(&arr[0], &arr[4]);
