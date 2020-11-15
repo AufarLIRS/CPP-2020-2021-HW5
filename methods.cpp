f74e3fa366d8ab813b37579f48ae943fd462cfe8
ZeByR0 <mezin.2001@gmail.com>
2020-11-15 13:42:24 +0300

    deleted empty files

--- methods.cpp
+++
@@ -1,6 +0,0 @@
-#include "methods.h"
-
-methods::methods()
-{
-
-}
--- task1.cpp
+++ task1.cpp
@@ -1,5 +1,5 @@
 #include "task1.h"
 int task1::minElement(int* array, int size)
 {
-  return *(std::min_element(array, array + size));
+  return *(std::min(array, array + size));
 }
