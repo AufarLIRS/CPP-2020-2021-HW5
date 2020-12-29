5b1e7ab81fb01b722de4b1dbdc7f373d2d26e95f
APGDiamond <shalmaz1733@gmail.com>
2020-12-29 17:50:46 +0300

    HW5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
-using namespace std;
+#include "minelement.h"
+#include "secondminelement.h"
+#include "rotate.h"
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int a[] = { 182, -33, -5, 12, -2, 2, 7, 18 };
+    std::cout << "minlement: " << minElement(a, 9) << std::endl;
+    std::cout << "Second minelement: " << minElement_(&a[0], &a[8]) << std::endl;
+    rotate(&a[0], &a[8]);
+    for (int i = 0; i < 9; i++)
+    {
+        std::cout << a[i] << " ";
+    }
+  return 1;
 }
