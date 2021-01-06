83833dde7635ee34f6f26865977d1bd1b6a131de
CamiloFajardo200 <camilo.fajardo.2001@hotmail.com>
2021-01-06 17:24:49 +0300

    HW5

---
+++ task_3.h
@@ -0,0 +1,15 @@
+#ifndef TASK_3_H
+#define TASK_3_H
+
+#include <stdexcept>
+#include <algorithm>
+
+class Task_3
+{
+  Task_3();
+
+public:
+  static void rotate(int* first_elem, int* last_elem);
+};
+
+#endif  // TASK3_H
